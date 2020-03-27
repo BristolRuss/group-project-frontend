a896d3ed7f8efd186055f30411ef8c3a2260a82f
BristolRuss <BristolRuss@gmail.com>
2020-03-27 10:57:50 +0000

    Updates

---
+++ precache-manifest.18358607a95326670f051e31b004b761.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db0f9c98167e76182708a612ebdc5ee",
+    "url": "/group-project-frontend/index.html"
+  },
+  {
+    "revision": "1589080a10ead8c7c995",
+    "url": "/group-project-frontend/static/css/main.f88cb620.chunk.css"
+  },
+  {
+    "revision": "cef57fe60718f9bc17f4",
+    "url": "/group-project-frontend/static/js/2.a41795ca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/group-project-frontend/static/js/2.a41795ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1589080a10ead8c7c995",
+    "url": "/group-project-frontend/static/js/main.0a374111.chunk.js"
+  },
+  {
+    "revision": "814722ba1df791bca29b",
+    "url": "/group-project-frontend/static/js/runtime-main.9f8d027e.js"
+  },
+  {
+    "revision": "5a848c29bc4bed4a4d83183fe1ac45b5",
+    "url": "/group-project-frontend/static/media/assorted-wine-bottles-1283219.5a848c29.jpg"
+  }
+]);
